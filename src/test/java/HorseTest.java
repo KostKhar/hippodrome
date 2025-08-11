2048fb24e5742aaafd22b79cd8a47d3ea2bf349d
kostkhar <konstantin.kharik@onlinesup.com>
2025-08-11 21:10:58 +0300

    add tests for Horse.class

--- src/test/java/HorseTest.java
+++ src/test/java/HorseTest.java
@@ -1,13 +1,19 @@
-import com.sun.jdi.connect.Connector;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 class HorseTest {
 
 
@@ -79,54 +85,70 @@ class HorseTest {
 
     @ParameterizedTest
     @MethodSource("getDataForNegativeDistance")
-    void distanceIsNegative_ShouldReturnIllegalArgumentException(double speed) {
+    void distanceIsNegative_ShouldReturnIllegalArgumentException(double distance) {
         IllegalArgumentException e = assertThrows(IllegalArgumentException.class,
-                ()-> new Horse("Sprinter", speed, 10.1)
+                ()-> new Horse("Sprinter", 2.4, distance)
         );
 
         assertEquals("Distance cannot be negative.", e.getMessage());
     }
 
+    private String name = "Sprinter";
+    private double speed = 5.2;
+    private double distance = 5.2;
+    private Horse horse = new Horse(name, speed, distance);
+
+
     @Test
     public void getName_ShouldReturnName(){
-        String name = "Sprinter";
-        double speed = 5.2;
-        double distance = 5.2;
-
-        Horse horse = new Horse(name, speed, distance);
-
         assertEquals(name, horse.getName());
     }
 
     @Test
     public void getSpeed_ShouldReturnSpeed(){
-        String name = "Sprinter";
-        double speed = 5.2;
-        double distance = 5.2;
-
-        Horse horse = new Horse(name, speed, distance);
-
         assertEquals(speed, horse.getSpeed());
     }
 
     @Test
     public void getName_ShouldReturnDistance(){
-        String name = "Sprinter";
-        double speed = 5.2;
-        double distance = 5.2;
-
-        Horse horse = new Horse(name, speed, distance);
-
         assertEquals(distance, horse.getDistance());
     }
 
 
-
     @Test
-    void move() {
+    void move_shouldReturnVerifyGetRandomDouble() {
+       try( MockedStatic<Horse> mockStatic =  Mockito.mockStatic(Horse.class)) {
+           mockStatic.when(() -> Horse.getRandomDouble(0.2, 0.9)).thenReturn(0.5);
+
+           horse.move();
+
+           mockStatic.verify(() -> Horse.getRandomDouble(0.2, 0.9));
+       }
     }
 
-    @Test
-    void getRandomDouble() {
+    public static Stream<Arguments> getDataForGetRandomInt() {
+        return Stream.of(
+                Arguments.arguments(0.21),
+                Arguments.arguments(0.5),
+                Arguments.arguments(0.34),
+                Arguments.arguments(0.89)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("getDataForGetRandomInt")
+    void move_shouldReturnCorrectDistance(double random) {
+        try(MockedStatic<Horse> mockStatic =  Mockito.mockStatic(Horse.class)){
+            mockStatic.when(() -> Horse.getRandomDouble(0.2, 0.9)).thenReturn(random);
+            double speed = 2.3;
+            double distance = 10.5;
+
+            Horse horse = new Horse("Funtic", speed, distance);
+
+            horse.move();
+            double expectedDistance =  distance + speed * random;
+
+            Assertions.assertEquals(expectedDistance,  horse.getDistance() ); ;
+        }
     }
 }
